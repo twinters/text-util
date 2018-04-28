76f3b2a24a16b4a785cb9f9615e2277d4fef89de
T. Winters <info@thomaswinters.be>
2018-04-28 22:23:16 +0200

    Added reading wordcounters from urls

--- src/main/java/be/thomaswinters/wordcounter/io/WordCounterIO.java
+++ src/main/java/be/thomaswinters/wordcounter/io/WordCounterIO.java
@@ -1,11 +1,13 @@
 package be.thomaswinters.wordcounter.io;
 
+import be.thomaswinters.util.DataLoader;
 import be.thomaswinters.wordcounter.WordCounter;
 import be.thomaswinters.wordcounter.WordCounter.Builder;
 import com.google.common.base.Charsets;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +25,19 @@ public class WordCounterIO {
     public static WordCounter read(File from) throws IOException {
         Builder b = new Builder();
         Files.readAllLines(from.toPath())
-                .forEach(e -> b.addWord(e.split("\t")[0], Integer.parseInt(e.split("\t")[1])));
+                .forEach(e -> add(b, e));
         return b.build();
+    }
+
+    private static void add(Builder b, String line) {
+        String[] splitted = line.split("\t", 2);
+        b.addWord(splitted[0], Integer.parseInt(splitted[1]));
+    }
+
+    public static WordCounter read(URL url) throws IOException {
+        Builder b = new Builder();
+        DataLoader.readLinesStream(url).forEach(e -> add(b, e));
+        return b.build();
+
     }
 }
