54077eb06cd44b710465cc6e009b9fccd5a996f7
T. Winters <info@thomaswinters.be>
2018-07-07 23:18:19 +0200

    Extended DropBeforeStringFixer by making it a set of strings instead of a single string

--- src/main/java/be/thomaswinters/text/fixers/DropBeforeStringFixer.java
+++ src/main/java/be/thomaswinters/text/fixers/DropBeforeStringFixer.java
@@ -1,21 +1,36 @@
 package be.thomaswinters.text.fixers;
 
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.Set;
+
 /**
  * Drops everything from the string before the given substring
  */
 public class DropBeforeStringFixer implements ISentenceFixer {
-    private final String dropUntilThisString;
+    private final Set<String> dropUntilThisString;
 
-    public DropBeforeStringFixer(String dropUntilThisString) {
+    public DropBeforeStringFixer(Set<String> dropUntilThisString) {
         this.dropUntilThisString = dropUntilThisString;
     }
 
+    public DropBeforeStringFixer(String... dropUntilThisString) {
+        this(Set.of(dropUntilThisString));
+    }
+
+
     @Override
     public String fix(String text) {
-        int substringIdx = text.indexOf(dropUntilThisString);
-        if (substringIdx > 0) {
-            return text.substring(substringIdx + dropUntilThisString.length());
+        Optional<String> firstString = dropUntilThisString
+                .stream()
+                .filter(text::contains)
+                .min(Comparator.comparingInt(text::indexOf));
+        if (firstString.isPresent()) {
+            int subStringIdx = text.indexOf(firstString.get());
+            if (subStringIdx > 0) {
+                return text.substring(subStringIdx + firstString.get().length());
+            }
         }
-        return "";
+        return text;
     }
 }
