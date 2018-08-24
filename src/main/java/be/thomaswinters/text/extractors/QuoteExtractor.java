2ca4a03ea1c8c5ea5449f36902197dbb3ae4da72
Thomas Winters <info@thomaswinters.be>
2018-08-24 18:56:28 +0200

    Added basic quote extractor

---
+++ src/main/java/be/thomaswinters/text/extractors/QuoteExtractor.java
@@ -0,0 +1,47 @@
+package be.thomaswinters.text.extractors;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class QuoteExtractor {
+    private final String quoteRegex;
+    private final Pattern quotePattern;
+
+    public QuoteExtractor(int minNumberWordsInQuote) {
+        quoteRegex = createQuoteRegex(minNumberWordsInQuote);
+        quotePattern = Pattern.compile(quoteRegex);
+    }
+
+    /**
+     * Creates a regex that recognises a certain amount of words
+     */
+    private static String createQuoteRegex(int amountOfWords) {
+        return "\\\"(.+\\s+){" + (amountOfWords - 1) + ",}.+\\\"";
+    }
+
+    /**
+     * Finds and returns all matches found by a particular Matcher object
+     */
+    public static List<String> getAllMatches(Matcher m) {
+        m.reset();
+        List<String> allMatches = new ArrayList<>();
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        return allMatches;
+    }
+
+    public boolean hasQuotes(String text) {
+        return quotePattern.matcher(text).find();
+    }
+
+    public String removeQuotes(String text) {
+        return text.replaceAll(quoteRegex, "");
+    }
+
+    public List<String> getAllMatches(String text) {
+        return getAllMatches(quotePattern.matcher(text));
+    }
+}
