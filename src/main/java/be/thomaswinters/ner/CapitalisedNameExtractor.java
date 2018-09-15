2b0fe83a1ebdda876ef3d6d4515e7cd2d22ee881
Thomas Winters <info@thomaswinters.be>
2018-09-15 03:13:58 +0200

    Improved how capitalised name extractor sums

--- src/main/java/be/thomaswinters/ner/CapitalisedNameExtractor.java
+++ src/main/java/be/thomaswinters/ner/CapitalisedNameExtractor.java
@@ -4,6 +4,7 @@ import be.thomaswinters.ner.data.NameAnalysis;
 import be.thomaswinters.sentence.SentenceUtil;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,12 +14,13 @@ import java.util.stream.Stream;
 
 public class CapitalisedNameExtractor {
 
-    private static final Set<String> PROHIBITED_NAMES = Set.of("de", "het", "van", "voor");
+    private static final Set<String> PROHIBITED_NAMES = Set.of("de", "het", "van", "voor", "ze", "volgens", "ik", "we","jullie", "je", "hij", "zij");
 
     public Multiset<String> findNames(String text) {
         return processSentencesNameAnalysises(SentenceUtil.splitIntoSentences(text)
                 .stream()
                 .flatMap(e -> Stream.of(e.split("\n")))
+                .flatMap(e -> Stream.of(e.split("\"")))
                 .map(this::findNamesInSentence)
                 .collect(Collectors.toList()));
 
@@ -29,17 +31,23 @@ public class CapitalisedNameExtractor {
 
         // Add all "normal" names: they're safe
         Multiset<String> result = HashMultiset.create(nameAnalysis.getNames());
+        System.out.println("\n\n********************************\nSTARTING FINAL AGGREGATE ANALYSIS:");
+        System.out.println("CERTAIN NAMES: " + Multisets.copyHighestCountFirst(result));
 
         // Only add "potentially too long names" if they are already in the names set:
+        System.out.println("\n\n\nEXAMINING LONG NAMES: " + Multisets.copyHighestCountFirst(nameAnalysis.getPotentiallyTooLongNames()));
         for (String name : nameAnalysis.getPotentiallyTooLongNames()) {
             if (result.contains(name)) {
                 result.add(name);
             }
         }
+        System.out.println("ADDED POTENTIALLY TOO LONG NAMES TO RESULT: " + Multisets.copyHighestCountFirst(result) + "\n\n\n");
 
         // Only add "begin of sentence" words if they're already part of something in the names set:
+        System.out.println("\n\n\nEXAMINING BEGIN WORDS: " + Multisets.copyHighestCountFirst(nameAnalysis.getBeginWords()));
+        Multiset<String> beginWordsToAdd = HashMultiset.create();
         for (String word : nameAnalysis.getBeginWords()) {
-            if (!PROHIBITED_NAMES.contains(word)) {
+            if (!PROHIBITED_NAMES.contains(word.toLowerCase())) {
                 // Get all names containing this word
                 List<String> namesContainingWord = result
                         .stream()
@@ -47,9 +55,15 @@ public class CapitalisedNameExtractor {
                         .collect(Collectors.toList());
 
                 // Add all these names
-                result.addAll(namesContainingWord);
+                if (!namesContainingWord.isEmpty()) {
+                    System.out.println("adding " + word + " containers -> " + namesContainingWord);
+                }
+                beginWordsToAdd.addAll(namesContainingWord);
             }
         }
+        System.out.println("ADDED BEGIN WORDS : " + Multisets.copyHighestCountFirst(beginWordsToAdd) + "\n\n");
+        result.addAll(beginWordsToAdd);
+        System.out.println("Current Result: " + Multisets.copyHighestCountFirst(result) + "\n\n");
 
         // Increase the count of names that are longer than other names
         result = increaseLongerNamesCount(result);
@@ -77,16 +91,20 @@ public class CapitalisedNameExtractor {
 
 
     private NameAnalysis findNamesInSentence(String sentence) {
+        sentence = sentence.trim();
         System.out.println("DEALING WITH SENTENCE: " + sentence);
         List<String> names = new ArrayList<>();
         List<String> potentiallyTooLongNames = new ArrayList<>();
         List<String> beginWords = new ArrayList<>();
 
-        List<String> words = SentenceUtil.getWords(sentence.trim());
+        List<String> words = SentenceUtil
+                .getWordsStream(sentence)
+                .filter(e -> e.trim().length() > 0)
+                .collect(Collectors.toList());
 
         int start = 1;
         // Add first capitalized words only if second word is capitalized
-        if (isPotentialName(words.get(0))) {
+        if (!words.isEmpty() && isPotentialName(words.get(0))) {
             if (words.size() >= 2 && isPotentialName(words.get(1))) {
                 StringBuilder name = new StringBuilder(words.get(0));
                 while (start < words.size() && isPotentialName(words.get(start))) {
